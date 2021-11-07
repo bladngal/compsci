8532bbe66aea0ba0952f5ea9c243daaf44586e85
Laura Maher <laura@maher-family.net>
2021-11-07 11:45:49 -0800

    removing test

--- src/tests/areThereDuplicates.test.js
+++
@@ -1,6 +0,0 @@
-import {areThereDuplicates} from '../areThereDuplicates';
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(3).toBe(3);
-    expect(areThereDuplicates('a', 'b', 'c', 'a')).toBe(true);
-  });
