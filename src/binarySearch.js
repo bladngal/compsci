9596cc8157e9c5f368825866227729121eba3b84
Laura Maher <laura@maher-family.net>
2022-01-06 17:52:53 -0800

    Clean up and added test case

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -5,10 +5,7 @@ function binarySearch(arr, target) {
     var rightPointer = arr.length - 1;
     var testPointer;
     while(leftPointer < rightPointer){
-        //console.log('leftPointer', leftPointer);
-        //console.log('rightPointer', rightPointer);
         testPointer = leftPointer + Math.floor((rightPointer - leftPointer)/2);
-        //console.log('testPointer', testPointer);
         
         if(arr[testPointer] === target) return testPointer;
         //if not found, move a pointer
@@ -28,19 +25,16 @@ function binarySearch(arr, target) {
     return -1;
 }
 
-//console.log(binarySearch([1,2,3,4,5], 2)); // 1
-//console.log(binarySearch([1,2,3,4,5], 3)); // 2
-//console.log(binarySearch([1,2,3,4,5], 5)); // 4
-//console.log(binarySearch([1,2,3,4,5], 6)); // -1
-console.log(binarySearch([
-    5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+console.log(binarySearch([1,2,3,4,5], 2)); // 1
+console.log(binarySearch([1,2,3,4,5], 3)); // 2
+console.log(binarySearch([1,2,3,4,5], 5)); // 4
+console.log(binarySearch([1,2,3,4,5], 6)); // -1
+console.log(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
     40, 44, 64, 79, 84, 86, 95, 96, 98, 99
   ], 10)); // 2
-  console.log(binarySearch([
-    5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  console.log(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
     40, 44, 64, 79, 84, 86, 95, 96, 98, 99
   ], 95)); // 16
-  console.log(binarySearch([
-    5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  console.log(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
     40, 44, 64, 79, 84, 86, 95, 96, 98, 99
   ], 100) );// -1
