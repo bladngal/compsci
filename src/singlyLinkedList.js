ff95a2bd8a821ab13915d271a127f8852eee7197
Laura Maher <laura@maher-family.net>
2022-02-10 18:24:22 -0800

    Added push, pop, shift and unshift functionality

--- src/singlyLinkedList.js
+++ src/singlyLinkedList.js
@@ -25,16 +25,67 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+
+    pop() {
+        if(!this.head) return undefined;
+        var last = this.tail;
+        var prev = this.head;
+        let current = this.next;
+        while(current){
+            prev = current;
+            current = current.next;
+        }
+        this.tail = prev;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return last;
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+
+        let current = this.head;
+        this.head = this.head.next;
+        this.length--;
+
+        return current;
+    }
+
+    unshift(val){
+        let newHead = new Node(val);
+        if(!this.head){
+            this.head = newHead;
+            this.tail = newHead;
+        }
+        else{
+            newHead.next = this.head;
+            this.head = newHead;
+        }
+        this.length++;
+
+        return this;
+    }
 }
 
 function testProgram(){
     let myList = new SinglyLinkedList();
     myList.push("hello");
-    console.log(myList);
+   // console.log(myList);
     myList.push("cruel");
-    console.log(myList);
+    //console.log(myList);
     myList.push("world");
+    //console.log(myList);
+    //console.log(myList.pop());
+   // console.log(myList);
+    myList.unshift("why");
     console.log(myList);
+    myList.shift();
+    console.log(myList);
+
 }
 
 testProgram();
