4cd744c74c2e6f8061adc52a8aa8740e35781ee7
Laura Maher <laura@maher-family.net>
2022-02-17 17:48:57 -0800

    Added reverse() method and removed log statments

--- src/singlyLinkedList.js
+++ src/singlyLinkedList.js
@@ -12,6 +12,7 @@ class SinglyLinkedList{
         this.length = 0;
     }
 
+    //add a new node at the end of the list
     push(val) {
         let tempNode = new Node(val);
         if(!this.head){
@@ -26,6 +27,7 @@ class SinglyLinkedList{
         return this;
     }
 
+    //remove the last node of the list
     pop() {
         if(!this.head) return undefined;
         var last = this.tail;
@@ -45,6 +47,7 @@ class SinglyLinkedList{
         return last;
     }
 
+    //remove node at the beginning of list
     shift(){
         if(!this.head) return undefined;
 
@@ -55,6 +58,7 @@ class SinglyLinkedList{
         return current;
     }
 
+    //insert node at the beginning of list
     unshift(val){
         let newHead = new Node(val);
         if(!this.head){
@@ -70,6 +74,7 @@ class SinglyLinkedList{
         return this;
     }
 
+    //return the node at index idx
     get(idx){
         if(idx<0 || idx >= this.length){
             return null;
@@ -83,6 +88,17 @@ class SinglyLinkedList{
         return idxNode;
     }
 
+    //just console.log all the values
+    print(){
+        let current = this.head;
+        
+        while(current){
+            console.log(current.val);
+            current = current.next; 
+        }
+    }
+
+    //set the value of node at index idx to val
     set(idx, val){
 
         let setNode = this.get(idx);
@@ -93,6 +109,88 @@ class SinglyLinkedList{
         }
         //if get() returned null, then this set did not work!
         return false;
+    }
+
+    //inserve a node with value val indo index idx
+    //return true if it worked, false if it didn't
+    insert(idx, val) {
+        //invalid values
+        if(idx < 0 || idx > this.length) return false;
+
+        //if index of 0, just use the unshift() method
+        if(idx === 0){
+            this.unshift(val);
+            if(this) return true;
+            else return false;
+        }
+
+        //if index of length, you just add to the end using push()
+        if(idx === this.length){
+            this.push(val);
+            if(this) return true;
+            else return false;
+        }
+        else{
+            let prevNode = this.get(idx-1);
+            var isValid = false;
+            if(prevNode) isValid = true;
+            let newNode = new Node(val);
+            newNode.next = prevNode.next;
+            prevNode.next = newNode;
+            length++;
+            return isValid;
+        }
+    }
+
+    //remove the node at index idx
+    remove(idx){
+        //invalid values
+        if(idx < 0 || idx > this.length-1) return false;
+
+         //if index of 0, just use the shift() method
+        if(idx === 0){
+            this.shift();
+            if(this) return true;
+            else return false;
+        }
+        //if index of length-1, you just remove from the end using pop()
+        if(idx === this.length-1){
+            this.pop();
+            if(this) return true;
+            else return false;
+        }
+            
+        //get the node before the one we want to take out
+        let prevNode = this.get(idx-1);
+        //then get the node to remove
+        let returnNode = prevNode.next;
+        //set the previous node's next attribute to the correct new next
+         prevNode.next = returnNode.next;
+        this.length--;
+        return returnNode;
+                
+    }
+
+    reverse(){
+            
+        let current = this.head;
+        let nextNode = current.next;
+        let nodeAfterNext = nextNode.next;
+        let origFirst = this.head;
+        current.next = null;
+       
+// why      hello      cruel        world        
+// current  nextNode   nodeAfterNext      
+        while(current != this.tail){
+            
+            nextNode.next = current;
+            current = nextNode;
+            
+            nextNode = nodeAfterNext;
+            if(nextNode.next) nodeAfterNext = nextNode.next;  
+        }
+        this.head = current;
+        this.tail = origFirst; //make the old head the new tail
     }
 }
 
@@ -107,12 +205,16 @@ function testProgram(){
     //console.log(myList.pop());
    // console.log(myList);
     myList.unshift("why");
-    console.log(myList);
-    myList.shift();
-    console.log(myList);
-    console.log(myList.get(1));
-    console.log(myList.set(0, "goodbye"));
-    console.log(myList);
+    //console.log(myList);
+    // myList.shift();
+    // console.log(myList);
+    // console.log(myList.get(1));
+    // console.log(myList.set(0, "goodbye"));
+   // myList.insert(4, "crazy");
+    myList.print();
+    myList.reverse();
+    //myList.remove(2);
+    myList.print();
 
 }
 
